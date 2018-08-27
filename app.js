bee79f92d1066aa16a061f392ae59bc0aea2cc1f
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-27 14:09:06 +0200

    Added JsonParser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require('express');
+const jsonParser = require('body-parser').json;
 const app = express();
+
+app.use(jsonParser());
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Express server is listening on port", port)
+    console.log('Express server is listening on port', port)
 });
