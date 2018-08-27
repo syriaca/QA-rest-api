5c535f36762c51a5b94ab4a81a9496e2c6af05f3
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-27 14:36:29 +0200

    Added morgan to get custom logs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+const routes = require('./routes');
 const jsonParser = require('body-parser').json;
+const logger = require('morgan');
 const app = express();
 
+app.use(logger('dev'));
 app.use(jsonParser());
+
+app.use('/questions', routes);
 
 const port = process.env.PORT || 3000;
 
