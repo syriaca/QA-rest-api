133eab093e23c67f1989ad5d6b95a9b6b856cf9e
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-27 15:11:02 +0200

    Error handling

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// catch 404 and forward to error handler
+app.use((req, res, next)=> {
+   let err = new Error('Not found'); 
+   err.status = 404;
+   next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // GET /questions
 // Route for questions collection
 router.get('/', (req, res) => {
+    var o = obj.prop;
     res.json({
         response: 'You sent me a GET request'
     });
@@ -22,11 +23,61 @@ router.post('/', (req, res) => {
 
 // GET /questions/:id
 // Route for specific questions
-router.get('/:id', (req, res) => {
+router.get('/:qID', (req, res) => {
     res.json({
-        response: 'You sent me a GET request for ID' + req.params.id
+        response: 'You sent me a GET request for ID' + req.params.qID
     });
 });
 
+// POST /questions/:qID/answers
+// Route for creating an answer
+router.post('/:qID/answers', (req, res) => {
+    res.json({
+        response: 'You sent me a POST request to/answers',
+        questionId: req.params.qID,
+        body: req.body
+    });
+});
+
+// PUT /questions/:qID/answers/:aID
+// Edit a specific answer
+router.put('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: 'You sent me a PUT request to /answers',
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// Delete a specific answer
+router.delete('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: 'You sent me a DELETE request to /answers',
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+    });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// Vote on a specific answer
+router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
+        if(req.params.dir.search(/^(up|down)$/) === -1) {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        } else {
+            next();
+        }
+    },(req, res) => {
+        res.json({
+            response: 'You sent me a DELETE request to/vote-' + req.params.dir,
+            questionId: req.params.qID,
+            answerId: req.params.aID,
+            vote: req.params.dir
+        });
+});
 
 module.exports = router;
