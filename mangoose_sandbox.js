956f16f82e78de94ef4a95ea3e7209729c7a7d95
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-28 12:32:41 +0200

    Added schema methods
    Added method to filter out result when making the query

--- mangoose_sandbox.js
+++ mangoose_sandbox.js
@@ -18,16 +18,36 @@ db.once('open', () => {
     let AnimalSchema =  new Schema({
         type:   {type: String, default: 'goldfish'},
         color:  {type: String, default: 'golden'},
-        size:   {type: String, default: 'small'},
+        size:   String,
         mass:   {type: Number, default: 0.007},
         name:   {type: String, default: 'Angela'}
     });
+
+    AnimalSchema.pre('save', function(next) {
+        if(this.mass >= 100) {
+            this.size = 'big'
+        } else if (this.mass >= 6 && this.mass < 1000) {
+            this.size = 'medium'
+        } else {
+            this.size = 'small'
+        }
+        next();
+    });
+
+    AnimalSchema.statics.findSize = (size, callback) => {
+        // This == Animal
+        return Animal.find({size: size}, callback);
+    };
+
+    AnimalSchema.methods.findSameColor = function(callback) {
+        // this == document
+        return this.model('Animal').find({color: this.color});
+    };
 
     let Animal = mongoose.model('Animal', AnimalSchema);
 
     let elephant = new Animal({
         type: 'elephant',
-        size: 'big',
         color: 'gray',
         mass: 6000,
         name: 'Lawrence'
@@ -37,31 +57,48 @@ db.once('open', () => {
     
     let whale =  new Animal({
         type: 'whale',
-        size: 'big',
         mass: 190500,
         name: 'Fig'
     });
 
+    let animalData = [
+        {
+            type: 'mouse',
+            color: 'gray',
+            mass: 0.035,
+            name: 'Marvin'
+        },
+        {
+            type: 'nutria',
+            color: 'brown',
+            mass: 6.35,
+            name: 'Gretchen'
+        },
+        {
+            type: 'wolf',
+            color: 'gray',
+            mass: 45,
+            name: 'Iris'
+        },
+        elephant, 
+        animal,
+        whale
+    ];
+
     Animal.remove({}, function(err){
         if(err) console.error(err);
-        elephant.save((err) => {
-            if(err) console.error(err);
-            animal.save((err) => {
+            Animal.create(animalData, (err, animals) => {
                 if(err) console.error(err);
-                whale.save((err)=> {
-                    if(err) console.error(err);
-                    // Query animals with siz "big"
-                    Animal.find({size: 'big'}, (err, animals) => {
-                        animals.forEach((animal) => {
-                            console.log(animal.name + ' the ' + animal.color + ' ' + animal.type);
-                        });                                     
-                        db.close(() => {
-                            console.log('db connection closed');
-                        });
-                    });   
-                });
+                Animal.findSize('medium', (err, animals) => {
+                    animals.forEach((animal) => {
+                        console.log(animal.name + ' the ' + animal.color + ' ' + animal.type + ' is a ' + animal.size + '-sized animal.');
+                    });                                     
+                    db.close(() => {
+                        console.log('db connection closed');
+                    });
+                });   
             });
-        }); 
-    });
-});
+      });
+}); 
+
 
