b6a272e52d2319f1cdbbf284d77a56471d022454
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-30 12:16:13 +0200

    Added CORS

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ db.once('open', () => {
     console.log('db connection successful');
 });
 
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Acceot');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/questions', routes);
 
 app.use((req, res, next)=> {
